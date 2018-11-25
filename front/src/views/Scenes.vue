ea9e54fff408980d0e94b17004ef7f3e9132dc57
Léo Peltier <contact@leo-peltier.fr>
2018-11-25 19:14:57 +0100

    RTFM and to button groups the right way
    That's a lot of tags where a single &nbsp; worked though.

--- front/src/views/Scenes.vue
+++ front/src/views/Scenes.vue
@@ -23,12 +23,17 @@
           <td>{{ scene.VROMEnd | hex(8) }}</td>
           <td>{{ scene.VROMEnd - scene.VROMStart | humanizeBytes }}</td>
           <td>
-            <a class="button" :href="'/api/files/' + scene.VROMStart | apiURI">Download</a>
-            &nbsp;
-            <RouterLink
-              class="button is-primary"
-              :to="{name: 'SceneDetail', params: {start: scene.VROMStart}}"
-            >Details</RouterLink>
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="button" :href="'/api/files/' + scene.VROMStart | apiURI">Download</a>
+              </p>
+              <p class="control">
+                <RouterLink
+                  class="button is-primary"
+                  :to="{name: 'SceneDetail', params: {start: scene.VROMStart}}"
+                >Details</RouterLink>
+              </p>
+            </div>
           </td>
         </tr>
       </tbody>
